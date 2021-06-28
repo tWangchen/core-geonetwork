7d053e31dfab880aadde6f1a21897677e43e0bf4
Joseph John <Joseph.John@ga.gov.au>
2021-06-28 15:05:19 +1000

    add constraint responsible party

--- web-ui/src/main/resources/catalog/views/default/module.js
+++ web-ui/src/main/resources/catalog/views/default/module.js
@@ -501,5 +501,11 @@
         var regex = new RegExp(from, 'g');
         return input.replace(regex, to);
       };
-    }]);
+    }])
+    .filter('titleCase', function() {
+      return function(input) {
+        input = input || '';
+        return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      };
+    });
 })();
